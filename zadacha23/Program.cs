2bdac650987a29ab8bb39cd66673967f59566300
maxbondarenko <bondarenkomm@bk.ru>
2022-11-25 22:40:55 +0500

    zadacha23

---
+++ zadacha23/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    string value = Console.ReadLine();
+    return Convert.ToInt32(value);
+}
+int N = Prompt("Введите число ");
+for(int count = 0; count <= N; count++)
+{
+    int result = count*count*count;
+    System.Console.WriteLine(result);
+}
